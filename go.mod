61967609ab90e5f9b5bdcf1ea06b53a4982e8f64
wbartholomay <willbartholomay@gmail.com>
2025-06-18 09:07:18 -0400

    created initial repl

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wbarthol/ascii-arcade
+
+go 1.24.4
---
+++ repl.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type config struct {
+
+}
+
+type cliCommand struct {
+	name string
+	description string
+	callback func(cfg *config, params ...string) error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand {
+	}
+}
+
+func cleanInput(text string) []string {
+	text = strings.ToLower(text)
+	substrings := strings.Fields(text)
+	return substrings
+}
+
+//TODO: wrap this in an outer repl if more games than checkers exit
+func startRepl(cfg *config) {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Welcome to ASCII Checkers!\nEnter 'help' to see a list of commands.")
+
+	for {
+		fmt.Print("Checkers > ")
+		scanner.Scan()
+
+		t := scanner.Text()
+		input := cleanInput(t)
+		if len(input) == 0 { continue }
+
+		cmd, ok := getCommands()[input[0]]
+		if !ok{
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := cmd.callback(cfg, input[1:]...)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
